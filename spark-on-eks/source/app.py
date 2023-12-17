97c1eaf693a69f35bf61b24cbb6a0b9d33da7ae4
Melody <meloyang@amazon.com>
2023-12-18 03:34:37 +1100

    upgrade to eks1.27 and alb,ca

--- spark-on-eks/source/app.py
+++ spark-on-eks/source/app.py
@@ -11,14 +11,14 @@ eks_name = app.node.try_get_context('cluster_name')
 eks_stack = SparkOnEksStack(app, 'SparkOnEKS', eks_name)
 # The CloudFront offers a default domain name to enable HTTPS.
 # Recommend to issue a TLS certificate with your own domain, delete the CF nested stack 
-cf_nested_stack = NestedStack(eks_stack,'CreateCloudFront', eks_stack.code_bucket, eks_stack.argo_url, eks_stack.jhub_url)
+# cf_nested_stack = NestedStack(eks_stack,'CreateCloudFront', eks_stack.code_bucket, eks_stack.argo_url, eks_stack.jhub_url)
 
 Tags.of(eks_stack).add('project', 'sqlbasedetl')
-Tags.of(cf_nested_stack).add('project', 'sqlbasedetl')
+# Tags.of(cf_nested_stack).add('project', 'sqlbasedetl')
 
 # Deployment Output
 CfnOutput(eks_stack,'CODE_BUCKET', value=eks_stack.code_bucket)
-CfnOutput(eks_stack,'ARGO_URL', value='https://'+ cf_nested_stack.argo_cf)
-CfnOutput(eks_stack,'JUPYTER_URL', value='https://'+ cf_nested_stack.jhub_cf)
+# CfnOutput(eks_stack,'ARGO_URL', value='https://'+ cf_nested_stack.argo_cf)
+# CfnOutput(eks_stack,'JUPYTER_URL', value='https://'+ cf_nested_stack.jhub_cf)
 
 app.synth()
